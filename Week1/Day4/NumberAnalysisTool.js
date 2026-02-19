ab293ddb0969bb6aca818098445d7ca027bcaadc
varaprasadkorapati <varaprasadkorapati@gmail.com>
2026-02-19 21:57:41 +0530

    Day4 Task Completed

---
+++ Week1/Day4/NumberAnalysisTool.js
@@ -0,0 +1,14 @@
+let num = 7;
+
+let result = (num >= 0) ? "Positive" : "Negative";
+console.log(result);
+
+if (num % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+for (let i = 1; i <= num; i++) {
+    console.log(i);
+}
